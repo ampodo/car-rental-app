7af5120a22aaa282a9f3a6917747bc1c708d0008
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-08-21 22:38:07 +0300

    regex rules added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Button,
   Card,
@@ -5,8 +6,21 @@ import {
   Checkbox,
   Typography,
 } from "./MaterialTailwindComponents";
+import Link from "next/link";
 
 function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Card color="transparent" shadow={false} className="-mt-10">
@@ -23,21 +37,47 @@ function Register() {
             Please register to start renting cars with us
           </Typography>
         </div>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <form
+          onSubmit={handleSubmit}
+          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+        >
           <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" label="Name" />
-            <Input size="lg" label="Email" />
-            <Input type="password" size="lg" label="Password" />
+            <Input
+              size="lg"
+              label="Name"
+              name="name"
+              type="text"
+              pattern="[A-Za-z]{3,}"
+              title="Please enter at least 3 letters"
+              required
+            />
+            <Input
+              type="email"
+              size="lg"
+              label="Email"
+              name="email"
+              pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+              required
+            />
+            <Input
+              type="password"
+              size="lg"
+              label="Password"
+              name="password"
+              pattern="^(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z!@#$%^&*\d]{8,}$"
+              title="Password must contain at least 1 capital letter, 1 symbol, and be at least 8 characters long."
+              required
+            />
           </div>
 
-          <Button className="mt-6" fullWidth>
+          <Button type="submit" className="mt-6" fullWidth>
             Register
           </Button>
           <Typography color="gray" className="mt-4 text-center font-normal">
             Already have an account?{" "}
-            <a href="#" className="font-medium text-gray-900">
+            <Link href="/login" className="font-medium text-gray-900">
               Sign In
-            </a>
+            </Link>
           </Typography>
         </form>
       </Card>
