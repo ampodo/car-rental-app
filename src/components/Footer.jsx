86fd3e78431bd45c032f8243c62341d718be27ee
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-10-23 21:55:35 +0300

    project finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export function SimpleFooter() {
   return (
     <footer className="bg-white bg-opacity-80 flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t py-16 text-center md:justify-between my-6 mb-0">
       <Typography color="blue-gray" className="font-normal ml-4">
-        &copy; {currentYear} Car Rentals
+        &copy; {currentYear} Car Rental app
       </Typography>
     </footer>
   );
